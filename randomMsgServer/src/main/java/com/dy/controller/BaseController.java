164b8ee8d0f438f7b7773c16bbaae84c922d19b4
Daeyong Kim <kim2509@gmail.com>
2013-10-24 00:35:30 +0900

    get requeset as json

--- randomMsgServer/src/main/java/com/dy/controller/BaseController.java
+++ randomMsgServer/src/main/java/com/dy/controller/BaseController.java
@@ -1,17 +1,22 @@
 package com.dy.controller;
 
+import java.util.HashMap;
+
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.type.TypeReference;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dy.domain.User;
@@ -24,18 +29,33 @@ public class BaseController implements BeanFactoryAware {
 	@Autowired
 	private DataSource dataSource;
 	
-	@RequestMapping( value ="/getMetaInfo.do", method = RequestMethod.GET)
-	public @ResponseBody User getMetaInfo(ModelMap model, @RequestParam("userNo") String userNo) {
+	@RequestMapping( value ="/getMetaInfo.do", method = RequestMethod.POST)
+	public @ResponseBody User getMetaInfo(ModelMap model, @RequestBody String bodyString ) {
 		
 		User user = new User();
 		
-		if ( userNo == null || userNo.trim().equals(""))
+		try
 		{
-			sqlSession.insert("com.dy.mapper.insertUser", user );
+			HashMap<String,String> map = new HashMap<String,String>();
+			ObjectMapper mapper = new ObjectMapper();
+			
+			map = mapper.readValue(bodyString, 
+				    new TypeReference<HashMap<String,String>>(){});
+			
+			String userNo = map.get("USER_NO");
+			
+			if ( userNo == null || userNo.trim().equals(""))
+			{
+				sqlSession.insert("com.dy.mapper.insertUser", user );
+			}
+			else
+			{
+				user = sqlSession.selectOne( "com.dy.mapper.getUser",  userNo );
+			}
 		}
-		else
+		catch( Exception ex )
 		{
-			user = sqlSession.selectOne( "com.dy.mapper.getUser",  userNo );
+			
 		}
 		
 //		int userCount = sqlSession.selectOne("com.dy.mapper.getUserCount");
