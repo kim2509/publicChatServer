73ee74050113b5c55a5facc9579b444c849ed244
Daeyong Kim <kim2509@gmail.com>
2013-11-24 22:24:49 +0900

    paging

---
+++ randomMsgServer/src/main/java/com/dy/common/Constants.java
@@ -0,0 +1,7 @@
+package com.dy.common;
+
+public class Constants {
+
+	public final static int FETCH_MESSAGE_COUNT = 10;
+	
+}
---
+++ randomMsgServer/src/main/java/com/dy/domain/ChatMessage.java
@@ -0,0 +1,33 @@
+package com.dy.domain;
+
+public class ChatMessage {
+
+	private long roomID;
+	private String msg;
+	private String sender;
+	private String createDate;
+	public long getRoomID() {
+		return roomID;
+	}
+	public void setRoomID(long roomID) {
+		this.roomID = roomID;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+}
