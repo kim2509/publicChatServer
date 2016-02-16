c478c7d819ef7e1b10ac7c2a59dc9cda05b69ce0
김대용 <cansys949@corp.auction.co.kr>
2016-02-16 20:24:30 +0900

    update source

--- query.sql
+++ query.sql
@@ -1,15 +1,8 @@
-CREATE TABLE `push_job` (
-	`pushID` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`param` VARCHAR(50) NOT NULL,
-	`pushType` VARCHAR(50) NOT NULL,
-	`finishYN` VARCHAR(1) NULL DEFAULT NULL,
+CREATE TABLE `user_friends` (
+	`userNo` INT(11) NOT NULL,
+	`friendNo` INT(11) NOT NULL,
 	`createdDate` DATETIME NOT NULL,
-	`finishedDate` DATETIME NULL DEFAULT NULL,
-	PRIMARY KEY (`pushID`),
-	INDEX `createdDate` (`createdDate`),
-	INDEX `finishYN` (`finishYN`)
+	PRIMARY KEY (`userNo`, `friendNo`)
 )
-COLLATE='utf8_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=2
 ;
