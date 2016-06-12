96a098122763bd2101aceb1eb387eb175f3f0f9d
Daeyong Kim <kim2509@gmail.com>
2016-06-12 14:34:54 +0900

    update source

--- randomMsgServer/src/main/java/com/dy/common/Constants.java
+++ randomMsgServer/src/main/java/com/dy/common/Constants.java
@@ -4,13 +4,13 @@ public class Constants {
 
 	public final static int FETCH_MESSAGE_COUNT = 10;
 	
-	public static String CSS_PATH = "/nearhere/css/";
-	public static String IMAGE_PATH = "/nearhere/image/";
-	public static String JS_PATH = "/nearhere/js/";
+	public static boolean bReal = false;
+	
+	public static String CSS_PATH = "http://172.30.1.200:8080/nearhere/css/";
+	public static String IMAGE_PATH = "http://172.30.1.200:8080/nearhere/image/";
+	public static String JS_PATH = "http://172.30.1.200:8080/nearhere/js/";
 	public static String GCM_API_KEY = "AIzaSyAfDDYJvFo6EWjLJH9PsPYzhcZJke30B4A";
 	public static int push_retry_count = 5;
-
-	public static boolean bReal = false;
 	
 	public static String getServerURL()
 	{
