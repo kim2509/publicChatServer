8433c8d61f96a8cf8703c5245c274573d7d10745
Daeyong Kim <kim2509@gmail.com>
2016-06-12 14:40:03 +0900

    update source

--- randomMsgServer/src/main/java/com/dy/common/Constants.java
+++ randomMsgServer/src/main/java/com/dy/common/Constants.java
@@ -4,15 +4,14 @@ public class Constants {
 
 	public final static int FETCH_MESSAGE_COUNT = 10;
 	
-	public static boolean bReal = false;
+	public static boolean bReal = true;
 	
-	public static String CSS_PATH = "http://172.30.1.200:8080/nearhere/css/";
-	public static String IMAGE_PATH = "http://172.30.1.200:8080/nearhere/image/";
-	public static String JS_PATH = "http://172.30.1.200:8080/nearhere/js/";
+	public static String CSS_PATH = "http://www.hereby.co.kr/nearhere/css/";
+	public static String IMAGE_PATH = "http://www.hereby.co.kr/nearhere/image/";
+	public static String JS_PATH = "http://www.hereby.co.kr/nearhere/js/";
 	public static String GCM_API_KEY = "AIzaSyAfDDYJvFo6EWjLJH9PsPYzhcZJke30B4A";
 	public static int push_retry_count = 5;
 	
-	public static boolean bReal = true;
 	public static String getServerURL()
 	{
 //		return Constants.bReal ? "http://www.hereby.co.kr/nearhere" : "http://localhost:8080/nearhere";
