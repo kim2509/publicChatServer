fd180af8e6b7e1dd0472374235edac795a88ba1a
김대용 <cansys949@corp.auction.co.kr>
2016-06-21 17:36:18 +0900

    서울시청 scraping 추가

--- randomMsgServer/src/main/java/com/tessoft/nearhere/taxi/NewsController.java
+++ randomMsgServer/src/main/java/com/tessoft/nearhere/taxi/NewsController.java
@@ -1,6 +1,8 @@
 package com.tessoft.nearhere.taxi;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,8 +24,23 @@ public class NewsController extends BaseController{
 	public ModelAndView list ( HttpServletRequest request, HttpServletResponse response , 
 			String userID, ModelMap model ) throws IOException
 	{
-		Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-		Elements newsHeadlines = doc.select("#mp-itn b a");
+		Document doc = Jsoup.connect("http://m.seoul.go.kr/mw/topmenu.do").get();
+		Elements newsHeadlines = doc.select("#container .grid").get(0).select(".container").get(0).select("li a[class=ts_a]");
+		
+		ArrayList<HashMap<String, String>> items = new ArrayList<HashMap<String, String>>();
+		
+		for ( int i = 0; i < newsHeadlines.size(); i++ )
+		{
+			String title = newsHeadlines.get(i).html();
+			String link = newsHeadlines.get(i).attr("href");
+			HashMap<String, String> hash = new HashMap<String, String>();
+			hash.put("title", title);
+			hash.put("link", link);
+			items.add( hash );
+		}
+		
+		model.addAttribute("host", "http://m.seoul.go.kr/");
+		model.addAttribute("items", items );
 		
 		return new ModelAndView("news/list", model);
 	}
--- randomMsgServer/src/main/webapp/WEB-INF/pages/news/list.jsp
+++ randomMsgServer/src/main/webapp/WEB-INF/pages/news/list.jsp
@@ -1,12 +1,35 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.util.*"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<%
+	String host = request.getAttribute("host").toString();
+	ArrayList<HashMap<String, String>> items = (ArrayList<HashMap<String, String>>) request.getAttribute("items");
+
+%>
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
 <title>Insert title here</title>
 </head>
 <body>
+
+<%
+	for ( int i = 0; i < items.size(); i++ ) {
+		
+		HashMap<String,String> hash = items.get(i);
+		String link = hash.get("link");
+		
+		if ( link.startsWith("javascript:") ) continue;
+		
+		if ( link.toLowerCase().startsWith("http") == false )
+			link = host + link;
+%>
+		<a href="<%= link %>"><%= hash.get("title") %></a><br/>		
+<%
+	}
+%>
 
 </body>
 </html>
