77794d6bdaf34f4d0e7e051ee3a6f4334d98faf5
Daeyong Kim <kim2509@gmail.com>
2016-07-16 12:52:14 +0900

    add logging

--- randomMsgServer/src/main/java/com/tessoft/nearhere/taxi/TaxiController.java
+++ randomMsgServer/src/main/java/com/tessoft/nearhere/taxi/TaxiController.java
@@ -1158,10 +1158,16 @@ public class TaxiController {
 
 		try
 		{
-			String logIdentifier = requestLogging(request, bodyString);
-
 			HashMap hash = mapper.readValue(bodyString, new TypeReference<HashMap>(){});
 
+			String userID = "";
+			if ( hash != null && hash.get("userID") != null )
+				userID = hash.get("userID").toString();
+			
+			String postID = "";
+			if ( hash != null && hash.get("postID") != null )
+				postID = hash.get("postID").toString();
+			
 			Post post = sqlSession.selectOne("com.tessoft.nearhere.taxi.getPostDetail", hash);
 			
 			if ( post.getUser() == null )
@@ -1223,7 +1229,7 @@ public class TaxiController {
 				}
 			}
 			
-			logger.info( "RESPONSE[" + logIdentifier + "]: " + mapper.writeValueAsString(response) );
+			logger.info( "[getPostDetail.do] userID:" + userID + " postID:" + postID + " title:" + post.getMessage() );
 		}
 		catch( Exception ex )
 		{
